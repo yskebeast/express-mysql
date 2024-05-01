ce77cb05c290f27775b6e6077853265af5fa0788
yske.biz@gmail.com <yske.biz@gmail.com>
2024-05-01 21:07:02 +0900

    add: connect to mysql

--- src/index.mjs
+++ src/index.mjs
@@ -1,12 +1,31 @@
 import express from "express";
+import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
 
+const PORT = 3000;
 const app = express();
-const port = 3000;
+
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
+
+connection.connect((error) => {
+  if (error) {
+    console.error("Error connecting to MySQL: ", error);
+    return;
+  }
+
+  console.log("Success connecting to MySQL");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
