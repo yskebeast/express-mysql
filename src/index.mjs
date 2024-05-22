460aec09d8c1d7a17ed02d6a4057d441d3dc1128
yusuke <yske.biz@gmail.com>
2024-05-23 00:21:41 +0900

    add: adding data and filtering data

--- src/index.mjs
+++ src/index.mjs
@@ -3,31 +3,82 @@ import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
+import { randomPrefecture } from "./util/prefectures.mjs";
+import { randomAge } from "./util/prefectures.mjs";
+
 const PORT = 3000;
 const app = express();
 
-const getMysqlDataQuery = async () => {
-  const connection = await mysql.createConnection({
+const initDatabase = async () => {
+  return mysql.createConnection({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
   });
+};
+
+const createTableQuery = async () => {
   try {
+    const connection = await initDatabase();
     await connection.connect();
     console.log("Success connecting to MySQL");
-
-    const [results, fields] = await connection.query(`SELECT * FROM ${process.env.MYSQL_DATABASE}`);
-    console.log(results, fields);
+    await connection.end();
   } catch (err) {
     console.log(err);
   }
 };
 
-getMysqlDataQuery();
+const addData = async () => {
+  try {
+    const connection = await initDatabase();
+    await connection.connect();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+    const pref = await randomPrefecture();
+    const age = await randomAge();
+
+    await connection.query(
+      `INSERT INTO user (name, pref, age) VALUES ('長谷川三郎', '${pref}', ${age})`
+    );
+
+    const getTableData = await connection.query("select * from user");
+    console.log(getTableData[0]);
+
+    return getTableData[0];
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+createTableQuery();
+
+app.get("/", async (req, res) => {
+  try {
+    const data = await addData();
+    console.log(data);
+    const connection = await initDatabase();
+    await connection.end();
+    res.send(JSON.stringify(data));
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/saburo", async (req, res) => {
+  try {
+    const connection = await initDatabase();
+    await connection.connect();
+
+    const getSaburoData = await connection.query(
+      "SELECT * FROM user WHERE name = '長谷川三郎' ORDER BY age ASC"
+    );
+    console.log(getSaburoData[0]);
+    await connection.end();
+    res.send(JSON.stringify(getSaburoData[0]));
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.listen(PORT, () => {
