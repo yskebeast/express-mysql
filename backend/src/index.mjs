f297b43c59ad1c65fd99d8f58aa8d7bc3dfca5e0
yusuke <yske.biz@gmail.com>
2024-05-26 15:54:19 +0900

    add: send data to db

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -1,6 +1,7 @@
 import express from "express";
 import mysql from "mysql2/promise";
 import cors from "cors";
+import bodyParser from "body-parser";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -10,9 +11,11 @@ import { randomAge } from "./util/prefectures.mjs";
 const PORT = 3001;
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
     optionsSuccessStatus: 200,
   })
 );
@@ -37,40 +40,51 @@ const createTableQuery = async () => {
   }
 };
 
-const addData = async () => {
+// const addData = async () => {
+//   try {
+//     const connection = await initDatabase();
+//     await connection.connect();
+//     const pref = await randomPrefecture();
+//     const age = await randomAge();
+//     await connection.query(
+//       `INSERT INTO user (name, pref, age) VALUES ('長谷川三郎', '${pref}', ${age})`
+//     );
+//     const getTableData = await connection.query("select * from user");
+//     console.log(getTableData[0]);
+//     return getTableData[0];
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+const postData = async ({ name, age, pref }) => {
   try {
     const connection = await initDatabase();
     await connection.connect();
-
-    const pref = await randomPrefecture();
-    const age = await randomAge();
-
     await connection.query(
-      `INSERT INTO user (name, pref, age) VALUES ('長谷川三郎', '${pref}', ${age})`
+      `INSERT INTO user (name, pref, age) VALUES ('${name}', '${pref}', ${age})`
     );
-
     const getTableData = await connection.query("select * from user");
     console.log(getTableData[0]);
-
-    return getTableData[0];
+    connection.end();
+    return {
+      name,
+      age,
+      pref,
+    };
   } catch (err) {
     console.log(err);
+    // return err;
   }
 };
 
 createTableQuery();
 
-app.get("/", async (req, res) => {
-  try {
-    const data = await addData();
-    console.log(data);
-    const connection = await initDatabase();
-    await connection.end();
-    res.send(JSON.stringify(data));
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Internal Server Error");
-  }
+app.post("/", async (req, res) => {
+  const { name, pref, age } = req.body;
+  const result = await postData({ name, age, pref });
+
+  res.send(result);
 });
 
 app.get("/saburo", async (req, res) => {
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,104 @@
+"use client";
+import { useState, ChangeEventHandler, FormEventHandler } from "react";
+
+import { prefectures } from "@/utils/prefectures";
+
 export default function Home() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [pref, setPref] = useState(prefectures[0]);
+  const [result, setResult] = useState({ name: "", age: "", pref: "" });
+  const [error, setError] = useState(false);
+
+  const handleNameChange: ChangeEventHandler<HTMLInputElement> = (event) =>
+    setName(event.target.value);
+
+  const handleAgeChange: ChangeEventHandler<HTMLInputElement> = (event) =>
+    setAge(event.target.value);
+
+  const handlePrefChange: ChangeEventHandler<HTMLSelectElement> = (event) =>
+    setPref(event.target.value);
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
+    event.preventDefault();
+
+    const regex = /^[^0-9０-９\s!-/:-@[-`{-~]+$/g;
+    const nameCheck = regex.test(name);
+
+    if (!name.length) {
+      alert("名前は必須です");
+      return;
+    }
+    if (!age.length) {
+      alert("年齢は必須です");
+      return;
+    }
+    if (!nameCheck) {
+      alert("数字や記号は入力できません");
+      return;
+    }
+    if (!name.length || !age.length) {
+      alert("名前と年齢は必須です");
+      return;
+    }
+
+    try {
+      const req = await fetch("http://localhost:3001/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, age, pref }),
+      });
+
+      const res = await req.json();
+      console.log("success!", res);
+      setError(false);
+      setResult({ name: res.name, age: res.age, pref: res.pref });
+    } catch (error) {
+      console.log("failed", error);
+      setError(true);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>hello</h1>
+    <main>
+      <h1>登録画面</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          名前:
+          <input type="text" value={name} onChange={handleNameChange} />
+        </label>
+        <br />
+        <label>
+          年齢:
+          <input type="number" min="0" value={age} onChange={handleAgeChange} />
+        </label>
+        <br />
+        <label>
+          都道府県:
+          <select value={pref} onChange={handlePrefChange}>
+            {prefectures.map((pref) => (
+              <option key={pref} value={pref}>
+                {pref}
+              </option>
+            ))}
+          </select>
+        </label>
+        <br />
+        <button type="submit">登録</button>
+      </form>
+
+      <div>
+        {error && <p>エラーが発生しました</p>}
+        {error || (
+          <>
+            <p>名前: {result.name}</p>
+            <p>年齢: {result.age}</p>
+            <p>都道府県: {result.pref}</p>
+          </>
+        )}
+      </div>
     </main>
   );
 }
